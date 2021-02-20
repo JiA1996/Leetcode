e11eeb3afdec55e01519408d87d5da111048e0ec
A Ji <ja3802@nyu.edu>
2021-02-20 21:24:35 +0800

    'iterative deepening'

---
+++ Dfs/iterative_deepening/iter.py
@@ -0,0 +1,113 @@
+import collections
+import sys
+
+class Solution:
+
+    def __init__(self):
+        self.cost_map = collections.defaultdict(lambda: 0)
+        self.vertex_list = []
+        self.edge_pair = []
+        self.budget = 0
+        self.verbose = None
+        self.goal = 0
+
+    def readinput(self, filename):
+        file = open(filename)
+
+        # read budget and verbose
+        line = file.readline()
+        b, self.verbose = line.split()
+        self.budget = int(b)
+
+        # read vertext and corresponding cost
+        while 1:
+            line = file.readline()
+            if line == "\n":
+                break
+            vertex, cost = line.split()
+            self.vertex_list.append(vertex)
+            self.cost_map[vertex] = int(cost)
+
+        # read edges
+        while 1:
+            line = file.readline()
+            if not line:
+                break
+            self.edge_pair.append(line.split())
+
+    def check_goal(self, state):
+        for v1, v2 in self.edge_pair:
+            if v1 in state or v2 in state:
+                continue
+            return False
+        return True
+
+    def get_succsessor(self, state):
+
+        res = []
+
+        if not state:
+            for j in range(0, len(self.vertex_list)):
+                successor = state + [self.vertex_list[j]]
+                res.append(successor)
+            return res
+
+        curr = state[-1]
+
+        for i in range(0, len(self.vertex_list)):
+            if self.vertex_list[i] == curr:
+                break
+
+        for j in range(i+1, len(self.vertex_list)):
+            successor = state + [self.vertex_list[j]]
+            res.append(successor)
+        return res
+
+    def get_total_cost(self, state):
+        res = 0
+        for s in state:
+            res += self.cost_map[s]
+        return res
+
+    def dfs(self, state, depth, maxdepth):
+        if depth > maxdepth:
+            return
+
+        if self.verbose == 'V' and state:
+            cost = self.get_total_cost(state)
+            if cost > self.budget:
+                return
+            print('{} Cost = {}.'.format(" ".join(state), cost))
+            if self.check_goal(state):
+                print('Find solution {} Cost = {}.'.format(" ".join(state), cost))
+                self.goal = 1
+                return
+
+        successor = self.get_succsessor(state)
+        for s in successor:
+            self.dfs(s, depth + 1, maxdepth)
+            if self.goal:
+                return
+
+        return 0
+
+    def iter_deep(self):
+        self.readinput(sys.argv[1])
+        for i in range(1, 5):
+            print('Depth = {}'.format(i))
+            ans = self.dfs([], 0, i)
+            if self.goal:
+                break
+            print("\n")
+
+if __name__ == '__main__':
+    sol = Solution()
+    sol.iter_deep()
+
+
+
+
+
+
+
+
